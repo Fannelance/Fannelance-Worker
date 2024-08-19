ee66a95071f69faae90b5b30f066f089c0f59ec3
Amira Tarek <amirat737@gmail.com>
2024-08-19 10:37:16 +0300

    Update otp resend button

--- lib/services/verify_otp_service.dart
+++ lib/services/verify_otp_service.dart
@@ -1,7 +1,7 @@
-
 import 'package:fannelance_worker/core/constants.dart';
 import 'package:fannelance_worker/helpers/api_request.dart';
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class VerifyOtpService {
   Future<void> verifyOtpRequest({
@@ -21,6 +21,19 @@ class VerifyOtpService {
           nextPage,
         );
       },
+    );
+  }
+
+  Future<void> resendPhoneNumberRequest() async {
+    String? token = await kSecureStorage.read(key: 'token');
+    Map<String, dynamic> decodedToken = JwtDecoder.decode(token!);
+    String phoneNumber =
+        decodedToken.containsKey('phone') ? decodedToken['phone'] : '';
+
+    await ApiRequest().post(
+      url: 'user/check-phone',
+      data: {'phone': phoneNumber},
+      writeToken: true,
     );
   }
 }
